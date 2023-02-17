5c5fdf580665104e5cb303968c040a05c813ffa0
MoglyGit <mike-feist@t-online.de>
2023-02-17 10:29:51 +0100

    Funktioniert

--- AltVRoleplay/Commands.cs
+++ AltVRoleplay/Commands.cs
@@ -36,6 +36,13 @@ namespace AltVRoleplay
         {
             player.Emit("freezeMe", freeze);
             player.SendChatMessage("Du hast dich gefreezt");
+        }
+
+        [Command("test")]
+        public void CMD_Test(MyPlayer.Player player, bool freeze = true)
+        {
+            player.Emit("freezeMe", freeze);
+            player.SendChatMessage("Du hast dich gefreezt");
             player.GiveWeapon(AltV.Net.Enums.WeaponModel.CombatMG, 100, true);
             player.Armor = 100;
         }
