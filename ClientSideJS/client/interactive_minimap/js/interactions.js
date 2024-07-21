a9e65c7f5b4fdcbb764ccba262075cc79b6ee2c4
MoglyGit <123118519+MoglyGit@users.noreply.github.com>
2024-07-21 17:55:29 +0200

    Add files via upload

---
+++ ClientSideJS/client/interactive_minimap/js/interactions.js
@@ -0,0 +1,24 @@
+let item = null;
+let start = null;
+let startstyle = null;
+function say(node) {
+    if (node == item) return;
+    console.log(node.innerHTML);
+}
+function moveItem(img) {
+    if (item != null) return;
+    startstyle = img.style.position;
+    img.style.position = 'absolute';
+    start = img.parentNode;
+    document.body.appendChild(img);
+    item = img;
+}
+function stopItemMoveX() {
+    console.log('out');
+    if (item == null) return;
+    item.style.position = startstyle;
+    start.appendChild(item);
+    startstyle = null;
+    start = null;
+    item = null;
+}
---
+++ ClientSideJS/client/interactive_minimap/typescript.js
@@ -0,0 +1,43 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
+const url = `http://resource/client/interactive_minimap/gtav.html`;
+let view = null;
+let cameraControlsInterval = null;
+alt.onServer('test', ShowTest);
+function ShowTest() {
+    if (view == null) {
+        view = new alt.WebView(url);
+        cameraControlsInterval = alt.setInterval(handleControls, 0);
+        view.focus();
+        showCursor(true);
+    }
+}
+function handleControls() {
+    native.disableAllControlActions(0);
+    //native.disableAllControlActions(1);
+    native.disableControlAction(0, 0, true);
+    native.disableControlAction(0, 1, true);
+    native.disableControlAction(0, 2, true);
+    native.disableControlAction(0, 24, true);
+    native.disableControlAction(0, 25, true);
+}
+function showCursor(state) {
+    try {
+        alt.showCursor(state);
+    } catch (err) {
+        return;
+    }
+}
+function closeView() {
+    if (cameraControlsInterval !== undefined || cameraControlsInterval !== null) {
+        alt.clearInterval(cameraControlsInterval);
+        cameraControlsInterval = null;
+    }
+    if (view && view.destroy) {
+        view.destroy();
+    }
+    alt.deleteMeta('noInv');
+    alt.deleteMeta('noChat');
+    view = null;
+    showCursor(false);
+}
